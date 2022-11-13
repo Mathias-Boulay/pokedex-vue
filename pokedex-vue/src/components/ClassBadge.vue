366201f1c8b6db870752358576e3e03a07ca2805
Mathias-Boulay <mathiasboulay@free.fr>
2022-11-13 19:27:07 +0100

    Feat: Add a component that displays a pokemon category

---
+++ pokedex-vue/src/components/ClassBadge.vue
@@ -0,0 +1,46 @@
+<script setup>
+const props = defineProps({
+  pokemonSpecie: {
+    type: Object,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="pokemonClass badge">
+    <p class="legendaryPokemon" v-if="pokemonSpecie.is_mythical">Legendary Pokemon</p>
+    <p class="mythicalPokemon" v-if="pokemonSpecie.is_legendary">Mythical Pokemon</p>
+    <p class="babyPokemon" v-if="pokemonSpecie.is_mythical">Baby Pokemon</p>
+    <p class="normalPokemon" v-else>Normal Pokemon</p>
+  </div>
+</template>
+
+<style scoped>
+/* .pokemonClass {
+  width: min-content;
+  white-space: nowrap;
+  border-radius: 1rem;
+  overflow: hidden;
+} */
+
+p {
+  text-align: center;
+  padding: 0.5rem 1rem;
+  margin: 0;
+}
+.mythicalPokemon {
+  background-color: blueviolet;
+}
+.legendaryPokemon {
+  background-color: azure;
+}
+
+.babyPokemon {
+  background-color: pink;
+}
+
+.normalPokemon {
+  background-color: orange;
+}
+</style>
