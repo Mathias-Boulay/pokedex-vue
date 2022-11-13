856a786ae134586ee343a1dc20f9445aad19e328
Mathias-Boulay <mathiasboulay@free.fr>
2022-11-13 19:25:45 +0100

    Feat: add a component to display pokemon stats

---
+++ pokedex-vue/src/components/PokemonStats.vue
@@ -0,0 +1,25 @@
+<script setup>
+import StatBarVue from './StatBar.vue';
+
+const props = defineProps({
+  pokemon: Object
+});
+
+</script>
+
+<template>
+  <div class="pokemonStats">
+    <StatBarVue
+    v-for="stat in pokemon.stats"
+    :label="stat['stat']['name']"
+    :value="stat['base_stat']"
+    
+    />
+  </div>
+</template>
+
+<style scoped>
+  .pokemonStats {
+    
+  }
+</style>
---
+++ pokedex-vue/src/components/StatBar.vue
@@ -0,0 +1,38 @@
+<script setup>
+  const props = defineProps({
+    max: {
+      type: Number,
+      default: 100
+    },
+
+    value:{
+      type: Number,
+      required: true
+    },
+
+    label: {
+      type: String,
+      required: true
+    }, 
+
+  });
+</script>
+
+<template>
+  <div class="statBar">
+    <label :for="label"> {{ label }} : {{value}}</label>
+    <progress :id="label" :max="max" :value="value"> <!--Maybe add a slot ?--> </progress>
+  </div>
+</template>
+
+<style scoped>
+
+
+.statBar{
+  display: flex;
+  flex-direction: row;
+  justify-content: right;
+  gap: 1rem;
+}
+
+</style>
