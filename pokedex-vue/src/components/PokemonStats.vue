07bd5c401bc27e8326563d787c81e49d8fdb23ca
Mathias-Boulay <mathiasboulay@free.fr>
2022-11-15 22:02:28 +0100

    Style: Turn PokemonStats component into a card

--- pokedex-vue/src/components/PokemonStats.vue
+++ pokedex-vue/src/components/PokemonStats.vue
@@ -1,5 +1,6 @@
 <script setup>
 import StatBarVue from './StatBar.vue';
+import { getTypeColor } from '../assets/js/utils.js';
 
 const props = defineProps({
   pokemon: Object
@@ -8,18 +9,21 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="pokemonStats">
+  <div class="card pokemonStats">
     <StatBarVue
     v-for="stat in pokemon.stats"
     :label="stat['stat']['name']"
     :value="stat['base_stat']"
-    
+    :max="255"
+    :color="getTypeColor(pokemon.types[0].type.name)"
     />
   </div>
 </template>
 
 <style scoped>
-  .pokemonStats {
-    
-  }
+.pokemonStats {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
 </style>
