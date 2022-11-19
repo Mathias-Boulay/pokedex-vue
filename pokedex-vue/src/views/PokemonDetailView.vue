24d76def4aecc76c64601f6826dbe74904f5441f
Mathias-Boulay <mathiasboulay@free.fr>
2022-11-19 10:12:35 +0100

    Feat[DetailView]: Handle router parameter change
    This makes the page reactive to when someone checks out the previous evolution

--- pokedex-vue/src/views/PokemonDetailView.vue
+++ pokedex-vue/src/views/PokemonDetailView.vue
@@ -1,7 +1,8 @@
 
 <script setup>
+import { watch, ref } from 'vue';
 import { useRoute } from 'vue-router';
-import { pokedexInstance } from '../assets/js/utils.js';
+import { pokedexInstance } from '../assets/js/utils';
 import PokemonDescriptionVue from '../components/PokemonDescription.vue';
 import PokemonStatsVue from '../components/PokemonStats.vue';
 import SimpleCardVue from '../components/SimpleCard.vue';
@@ -12,20 +13,39 @@ import TypeDynamicsVue from '../components/TypeDynamics.vue';
 
 const route = useRoute();
 
-const param = route.params.name;
-const currentPokemon = await pokedexInstance.getPokemonByName(route.params.name);
-const currentPokemonSpecie = await pokedexInstance.getPokemonSpeciesByName(route.params.name);
+/* Current pokemon details */
+const currentPokemon = ref(null);
+const currentPokemonSpecie = ref(null);
+const types = ref(null);
 
-const previousPokemon = currentPokemonSpecie.evolves_from_species 
-    ? await pokedexInstance.getPokemonByName(currentPokemonSpecie.evolves_from_species.name) 
-    : undefined;
+/* If the pokemon is an evolution, the previous pokemon is stored here */
+const previousPokemon = ref(null);
 
-const types = await Promise.all(currentPokemon.types.map(async typeInstance => await pokedexInstance.getTypeByName(typeInstance.type.name)));
+await updatePokemon(route.params.name);
+
+// Handle changes in the displayed pokemon
+watch(() => route.params.name, async (newName) => {
+  console.log("compute new pokemon data");
+  await updatePokemon(newName);
+});
+
+async function updatePokemon(pokemonName){
+  currentPokemon.value = await pokedexInstance.getPokemonByName(pokemonName);
+  currentPokemonSpecie.value = await pokedexInstance.getPokemonSpeciesByName(pokemonName);
+
+  previousPokemon.value = currentPokemonSpecie.value.evolves_from_species 
+      ? await pokedexInstance.getPokemonByName(currentPokemonSpecie.value.evolves_from_species.name) 
+      : undefined;
+
+  types.value = await Promise.all(currentPokemon.value.types.map(async typeInstance => await pokedexInstance.getTypeByName(typeInstance.type.name)));
+}
+
+
 </script>
 
 <template>
   <div class="pokemonDetailView">
-    <p> {{ $route.params.name}} - {{ param }} </p>
+    <p> {{ $route.params.name}} </p>
     <ClassBadgeVue :pokemon-specie="currentPokemonSpecie" />
     <EvolutionBadgeVue :pokemon="previousPokemon" /> 
     <SimpleCardVue :pokemon="currentPokemon"/>
